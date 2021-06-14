3c21ab769207625e4f2c7502b4cc9d7c9e1a4445
srinivas kattamuri <srinivasguptha.k@gmail.com>
2021-06-14 09:49:17 +0530

    build config changes

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+
 const express = require('express');
 const app = express();
 
 const path = require('path');
-app.use(requireHTTPS);
+
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
