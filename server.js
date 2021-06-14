043fd4796a0e009c6fec8dee69dd78a54ba4c22b
srinivas kattamuri <srinivasguptha.k@gmail.com>
2021-06-14 10:17:27 +0530

    build config changes

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
-
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
+//Install express server
 const express = require('express');
-const app = express();
-app.use(requireHTTPS);
-
 const path = require('path');
 
+const app = express();
 
-app.use(express.static('./dist/portfolio-app'));
-app.listen(process.env.PORT || 8080);
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/portfolioApp'));
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/portfolio-app/'}
-);
+app.get('*', function(req,res) {
+  // Replace the '/dist/portfolioApp/index.html'
+  res.sendFile(path.join(__dirname + '/dist/portfolioApp/index.html'));
 });
-
-
-console.log('listening');
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
