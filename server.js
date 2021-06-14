7ba007ded78b45bdae668ddcf4f239fe2d19d082
srinivas kattamuri <srinivasguptha.k@gmail.com>
2021-06-14 10:01:48 +0530

    build config changes

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
 const app = express();
+app.use(requireHTTPS);
 
 const path = require('path');
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/portfolio-app'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile('index.html', {root: 'dist/portfolio-app/'}
+);
 });
+
 
 console.log('listening');
